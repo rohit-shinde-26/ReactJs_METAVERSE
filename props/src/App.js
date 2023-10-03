8cb7e6134759829adf2f6731385c61449691e654
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2023-10-03 08:56:39 +0000

    props has been added

---
+++ props/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Props from './Component/Props';
+
+function App() {
+  return (
+    <div className="App">
+      <Props name="ROHIT" age="20"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ props/src/Component/Props.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Props =(Props) => {
+    
+    return(
+       <>
+        <h1>{Props.name}</h1>
+        <h1>{Props.age}</h1>
+       </>
+    )
+}
+export default Props;
+
+// Props are send parent to child component for the passing props import firstly on parent 
